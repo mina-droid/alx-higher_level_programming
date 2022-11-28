03d21034256c112cd2d8b77f036fc0de11f0828b
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-11-28 22:47:41 +0200

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,19 +8,19 @@
 
 int check_cycle(listint_t *list)
 {
-listint_t * temp;
-if(!list)
-{
-return (0);
-}
-temp = list;
-while(temp->next != list && temp->next != NULL)
-{
-temp->next = temp->next->next;
-}
-if(temp->next == NULL)
-{
-return (0);
-}
-return(1);
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	if (!list)
+		return (0);
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+
+	return (0);
 }
