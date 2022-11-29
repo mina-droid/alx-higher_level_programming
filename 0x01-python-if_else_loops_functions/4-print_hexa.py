8cecd28fd22f1425f8ff5a9371acadd456af737d
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-11-30 01:56:35 +0200

    Update 4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-
-for i in range (0, 99):
-
-	if i == 98:
-
-		print(f"{i} = {hex(i)}", end="")
-
-	else:
-
-		print(f"{i} = {hex(i)}")
+"""Print numbers 0 to 98 in decimal and hexadecimal."""
+for number in range(0, 99):
+    print("{} = {}".format(number, hex(number)))
