db5283bfca2adb3f93cdfd4f8b50e424f5ec93de
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-11-30 02:00:11 +0200

    Update 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 
-
-
 def print_last_digit(number):
-
-	last_digit = int(repr(number)[-1])
-
-	print(f"{last_digit:d}", end="")
-
-	return last_digit
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
