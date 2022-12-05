864f150eddda6a6ff4505ac9a08ce76b5eaa6273
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-12-05 16:54:17 +0200

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    for c in my_string:
-        if c == 'c' or c == 'C':
-            c = ''
-    return my_string
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
