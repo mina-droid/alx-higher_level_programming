22d605cf25885ac3fc1e9a269e0a0080deb8bc69
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-12-05 20:24:57 +0200

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
+    for i in matrix:
+        for j in i:
             print("{:d}".format(col), end=" " if j != i[-1] else "")
         print()
