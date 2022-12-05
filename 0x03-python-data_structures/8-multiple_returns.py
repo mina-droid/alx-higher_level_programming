bf6bbca61fa5ea64ace283906eac8fefe6011799
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-12-05 20:30:43 +0200

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    my_tuple = ()
     if len(sentence) == 0:
-        return (0, "None")
-    first = sentence[0]
-    length = len(sentence)
-    tu = (length, first)
-    return tu 
+        my_tuple = 0, "None"
+    else:
+        my_tuple = len(sentence), sentence[0]
+    return my_tuple
