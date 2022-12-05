4bdc8ca85d704f2f724ca3b6ada8d16af1516eff
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-12-05 20:32:40 +0200

    Update 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     result_list = my_list.copy()
-    for i in range (len(result_list)):
+    for i in range(len(result_list)):
         if result_list[i] % 2 == 0:
             result_list[i] = True
         else:
             result_list[i] = False
-    return result_list       
+    return result_list
