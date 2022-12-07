3d717501886cbc08e62d95f1e0712639bd3b3bf4
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-12-08 00:53:11 +0200

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    uniq_set = set(my_list)
-    sum = 0
-    for i in uniq_set:
-       sum += i
-    return sum
+    uniq_list = set(my_list)
+    num = 0
+
+    for i in uniq_list:
+        num += i
+
+    return (num)
