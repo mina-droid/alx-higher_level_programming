0556d3574a3cd8e0fdf06bc7958dc1382c80af05
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-12-08 00:57:42 +0200

    Update 11-multiply_list_map.py

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
     new_list = my_list.copy()
-    new_list = list(map(lambda x: x * number, new_list))
-    return list(new_list)
+    return list(map(lambda x: x * number, new_list))
