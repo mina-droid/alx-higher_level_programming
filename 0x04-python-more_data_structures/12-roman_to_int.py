67d0fb3d587a62b6c4a7ce6cae190151726394fd
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-12-08 01:23:01 +0200

    final

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,8 @@
 # !/usr/bin/python3
-
-
 def roman_to_int(roman_string):
     if not roman_string or type(roman_string) is not str:
         return 0
-
-    roman_dict = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000,
-    }
+    roman_dict = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
     roman_integer = 0
     if len(roman_string) == 1:
         return roman_dict[roman_string]
